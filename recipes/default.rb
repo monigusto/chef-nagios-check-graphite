9d0be95a4259c6d2b7b09e88c88a0e98d3069a68
Patrick Debois <Patrick.Debois@jedi.be>
2012-10-01 22:34:21 +0300

    Updates checksum of version

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,7 +22,7 @@ include_recipe "nagios"
 
 git "/opt/check-graphite" do
   repository "git://github.com/monigusto/chef-nagios-check-graphite.git"
-  reference "0f522dcee37538f670ffb1134045fb9d4358dd13"
+  reference "8b8053b4eb52e11b780da50ffeff7fe827515dc0"
   action :sync
 end
 
